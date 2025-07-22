aa7ca6acc86b31c7553ed6ec5ec951bcd109520f
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-07-22 10:02:05 -0600

    Changes to Etech - E - Sketch Script Commiting changes so that I can clone this repo to my main computer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const container = document.querySelector("#sketchSpace");
 const newSquare = document.createElement("div");
 newSquare.className = "etchSquare";
+
+container.addEventListener("hover,")
