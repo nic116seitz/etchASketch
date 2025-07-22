498fbe64842d84d1fc328a16e5da60a3d5428902
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-07-22 14:56:15 -0600

    Changed Appearances Changed the style to reflect a cleaner UI for the page Still working on figuring out how to create multiple divs with minimal amount of code

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const container = document.querySelector("#sketchSpace");
 const newSquare = document.createElement("div");
+
+
 newSquare.className = "etchSquare";
 
-container.addEventListener("hover,")
+container.appendChild(newSquare)
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+container.appendChild(newSquare.cloneNode(true))
+
+container.addEventListener("mouseover", () => {
+  newSquare.style.backgroundColor = "lightblue"
+  newSquare.style.borderWidth = "5px"
+  newSquare.style.borderColor = "darkblue"
+
+
+})
