ef22a79a8da75a51d88ed31bf2beba532ac9006f
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-07-24 15:20:49 -0600

    Per Square ID Added Id generation per square Started creating a function that will assign a random color on a per square basis

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const drawspace = document.querySelector("#sketchSpace");
+const square = document.querySelector(".etchSquare")
+let squareNum = 0;
 
 let usrDimension = 16;
+
 
 function gridGen(dimension) {
  for (row = 0; row < dimension; row++) {
     for(column = 0; column < dimension; column++) {
       const etched = document.createElement("div");
       etched.classList.add(".etchSquare");
+      etched.setAttribute("id", `square${squareNum}`);
       drawspace.appendChild(etched)
+      squareNum++
     }
   }
 }
+
 
 function randomColor(){
   const hexLetters = "0123456789ABCDEF";
@@ -20,6 +26,8 @@ function randomColor(){
   }
   return color;
 }
+
+// square.addEventListener("hover", )
 
 gridGen(usrDimension);
 
